4b84a90a1be87a0293f0d89b17175f43984c39d8
Fahad <fahadmasood03@gmail.com>
2023-08-26 18:32:20 +0530

    Ep-2 code

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-// const heading = React.createElement("h1", { id: "blueColor" }, "Hello World");
-// console.log(heading);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
