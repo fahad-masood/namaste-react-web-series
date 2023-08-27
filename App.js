43633954abbc86ac61c368fcd95129c691b39fc7
Fahad <fahadmasood03@gmail.com>
2023-08-28 00:29:57 +0530

    Ep-3 full code

--- App.js
+++ App.js
@@ -5,11 +5,17 @@ import ReactDOM from "react-dom/client";
 
 const heading = <h1>Hi from the react element</h1>;
 
-// React Component
-
-const HeadingComponent = () => {
-  return <h1>Hi from a React Component</h1>;
-};
+// Component Composition -> Calling a component inside another component
+const Title = () => <h1>This is a title</h1>;
+const HeadingComponent = () => (
+  <>
+    {/* 3 ways to call a React component */}
+    <Title />
+    <Title></Title>
+    {Title()}
+    <h1>Hi from a React Component</h1>
+  </>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<HeadingComponent />);
