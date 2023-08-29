c50bfa8ae9816869aab77d6dffe7adcd2dc86841
Fahad <fahadmasood03@gmail.com>
2023-08-29 23:26:57 +0530

    Ep-4 header-code

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Element
+const Header = () => (
+  <div className="header">
+    <div className="logo">
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSutTvjP5Nx7u5vk-IZlmBsiIZIn4ZgTli70lW3i_KRw2FriVJ0YxtVctxFi56AgnqZgrk&usqp=CAU"
+        alt=""
+        srcset=""
+      />
+    </div>
+    <div className="nav-items">
+      <ul>
+        <li>Search</li>
+        <li>Offers</li>
+        <li>Cart</li>
+      </ul>
+    </div>
+  </div>
+);
 
-const heading = <h1>Hi from the react element</h1>;
-
-// Component Composition -> Calling a component inside another component
-const Title = () => <h1>This is a title</h1>;
-const HeadingComponent = () => (
-  <>
-    {/* 3 ways to call a React component */}
-    <Title />
-    <Title></Title>
-    {Title()}
-    <h1>Hi from a React Component</h1>
-  </>
+const AppLayout = () => (
+  <div className="app">
+    <Header />
+  </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
